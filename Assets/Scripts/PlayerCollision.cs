0a6075fbb75f75b54e2ecd89291322c289c4d6d4
OscarCurtoC <oscar.curto@hotmail.com>
2019-11-15 16:01:03 +0100

    cositas

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+  void OnCollisionEnter(Collision  collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle") {//tag obstacle per qualsevol obj. amb aquest tag
+            //you die
+            movement.enabled = false;
+        }
+    }
+   
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 150f;
+    public float sidewaysForce = 70f;
+
+
+
+    // Start is called before the first frame update
+   /* void Start()
+    {
+        rb.AddForce(0, 200, 10);
+    }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if ( Input.GetKey("d") )
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a") )
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
